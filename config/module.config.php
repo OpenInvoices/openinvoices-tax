2fbc83c76acb9038a78f5548141d11be32b1655d
jpg-consulting <opensource@jpg-consulting.es>
2017-12-18 22:50:33 +0100

    added i18n

--- config/module.config.php
+++ config/module.config.php
@@ -38,6 +38,15 @@ return [
             Controller\IndexController::class => InvokableFactory::class,
         ],
     ],
+    'translator' => [
+        'translation_file_patterns' => [
+            [
+                'type'     => 'gettext',
+                'base_dir' => __DIR__ . '/../language',
+                'pattern'  => '%s.mo',
+            ],
+        ],
+    ],
     'view_manager' => [
         'template_path_stack' => [
             __DIR__ . '/../view',
